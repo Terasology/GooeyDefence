991ee3d9575a09f5231878ab6ea6d6f729bb4b5a
Quinn Roberts <quinn@jelly-snakes.com>
2018-05-03 15:08:00 +1000

    Add a working skeleton for the world generation

---
+++ src/main/java/org/terasology/gooeyDefence/worldGeneration/GooeyDefenceWorldGenerator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gooeyDefence.worldGeneration;
+
+import org.terasology.engine.SimpleUri;
+import org.terasology.registry.In;
+import org.terasology.world.generation.BaseFacetedWorldGenerator;
+import org.terasology.world.generation.WorldBuilder;
+import org.terasology.world.generator.RegisterWorldGenerator;
+import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
+
+@RegisterWorldGenerator(id = "gooeyDefenceField", displayName = "Gooey Defence", description = "The world generator for the Gooey Defence gameplay module")
+public class GooeyDefenceWorldGenerator extends BaseFacetedWorldGenerator {
+
+    @In
+    private WorldGeneratorPluginLibrary worldGeneratorPluginLibrary;
+
+    public GooeyDefenceWorldGenerator(SimpleUri uri) {
+        super(uri);
+    }
+
+    @Override
+    protected WorldBuilder createWorld() {
+        return new WorldBuilder(worldGeneratorPluginLibrary)
+                .addProvider(new SurfaceHeightProvider())
+                .addRasterizer(new GooeyDefenceWorldRasterizer());
+    }
+}
---
+++ src/main/java/org/terasology/gooeyDefence/worldGeneration/SurfaceHeightProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gooeyDefence.worldGeneration;
+
+import org.terasology.math.geom.BaseVector2i;
+import org.terasology.math.geom.Rect2i;
+import org.terasology.world.generation.Border3D;
+import org.terasology.world.generation.FacetProvider;
+import org.terasology.world.generation.GeneratingRegion;
+import org.terasology.world.generation.Produces;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+
+@Produces({SurfaceHeightFacet.class})
+public class SurfaceHeightProvider implements FacetProvider{
+    @Override
+    public void setSeed(long seed) {
+
+    }
+
+    @Override
+    public void process(GeneratingRegion region) {
+        Border3D border = region.getBorderForFacet(SurfaceHeightFacet.class);
+        SurfaceHeightFacet facet = new SurfaceHeightFacet(region.getRegion(),border);
+
+        Rect2i processRegion = facet.getWorldRegion();
+        for (BaseVector2i position : processRegion.contents()) {
+            facet.setWorld(position, 10f);
+        }
+        region.setRegionFacet(SurfaceHeightFacet.class, facet);
+    }
+}
