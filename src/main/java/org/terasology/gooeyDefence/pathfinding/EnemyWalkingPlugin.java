63b9363847789106b4b553e8e5c144623370e154
Quinn Roberts <quinn@jelly-snakes.com>
2018-06-25 21:43:18 +1000

    Refactor checks in `EnemyWalkingPlugin` into seperate methods.

--- src/main/java/org/terasology/gooeyDefence/pathfinding/EnemyWalkingPlugin.java
+++ src/main/java/org/terasology/gooeyDefence/pathfinding/EnemyWalkingPlugin.java
@@ -27,26 +27,27 @@ public class EnemyWalkingPlugin extends WalkingPlugin {
 
     @Override
     public boolean isReachable(Vector3i to, Vector3i from) {
+        return !to.equals(from)
+                && (isHorizontallyReachable(to, from) || isVerticallyReachable(to, from))
+                && isWalkable(to, from)
+                && areAllBlocksPenetrable(to, from);
+    }
 
-        /* Check both positions are not the same */
-        if (to.equals(from)
-                /* We can go diagonally if it's horizontal */
-                || to.y == from.y && to.distanceSquared(from) > 2
-                /* We can only go directly up or down */
-                || to.y != from.y && to.distanceSquared(from) > 1
-                /* Check that at least one of the to or from are walkable */
-                || !(isWalkable(to) || isWalkable(from))) {
-            return false;
-        }
-
-        /* Check that all blocks passed through by this movement are penetrable */
-        for (Vector3i occupiedBlock : getOccupiedRegionRelative()) {
+    /**
+     * Checks that all the blocks the enemy will pass through are penetrable.
+     *
+     * @param to   The ending position
+     * @param from The starting position
+     * @return True if all the blocks are penetrable.
+     */
+    private boolean areAllBlocksPenetrable(Vector3i to, Vector3i from) {
+        for (Vector3i relativePosition : getOccupiedRegionRelative()) {
 
             /* The start/stop for this block in the occupied region */
-            Vector3i occupiedBlockTo = new Vector3i(to).add(occupiedBlock);
-            Vector3i occupiedBlockFrom = new Vector3i(from).add(occupiedBlock);
+            Vector3i occupiedRegionStart = new Vector3i(to).add(relativePosition);
+            Vector3i occupiedRegionEnd = new Vector3i(from).add(relativePosition);
 
-            Region3i movementBounds = Region3i.createBounded(occupiedBlockTo, occupiedBlockFrom);
+            Region3i movementBounds = Region3i.createBounded(occupiedRegionStart, occupiedRegionEnd);
             for (Vector3i block : movementBounds) {
                 if (!world.getBlock(block).isPenetrable()) {
                     return false;
@@ -54,5 +55,52 @@ public class EnemyWalkingPlugin extends WalkingPlugin {
             }
         }
         return true;
+    }
+
+    /**
+     * When travelling horizontally, the enemy can travel along all 8 horizonatal directions.
+     * That is, the enemy can travel diagonally as well as cardinally.
+     *
+     * @param to   The ending position
+     * @param from The starting position
+     * @return True if the movement is horizontally possible.
+     */
+    private boolean isHorizontallyReachable(Vector3i to, Vector3i from) {
+        return isMovementHorizontal(to, from) && to.distanceSquared(from) <= 2;
+    }
+
+    /**
+     * When travelling vertically, the enemy can only go directly up or down.
+     * That is, they cannot travel diagonally
+     *
+     * @param to   The ending position
+     * @param from The starting position
+     * @return True if the movement is vertically possible.
+     */
+    private boolean isVerticallyReachable(Vector3i to, Vector3i from) {
+        return !isMovementHorizontal(to, from) && to.distanceSquared(from) <= 1;
+    }
+
+    /**
+     * Check if the movement is walkable.
+     * <p>
+     *
+     * @param to   The ending position
+     * @param from The starting position
+     * @return True if the the movement is walkable
+     */
+    private boolean isWalkable(Vector3i to, Vector3i from) {
+        return isWalkable(to) || isWalkable(from);
+    }
+
+    /**
+     * Checks if the movement is strictly horizontal
+     *
+     * @param to   The ending position
+     * @param from The starting position
+     * @return True if both positions are at the same height.
+     */
+    private boolean isMovementHorizontal(Vector3i to, Vector3i from) {
+        return to.y == from.y;
     }
 }
