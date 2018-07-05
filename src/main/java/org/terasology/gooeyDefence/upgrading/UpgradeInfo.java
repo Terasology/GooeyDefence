446ffe029a48407f7c9677a0b5c3e30d366fb583
Quinn Roberts <quinn@jelly-snakes.com>
2018-07-05 13:19:26 +1000

    Add basic upgrader capable of increasing number fields on a component

---
+++ src/main/java/org/terasology/gooeyDefence/upgrading/UpgradeInfo.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gooeyDefence.upgrading;
+
+import org.terasology.reflection.MappedContainer;
+
+import java.util.Map;
+
+/**
+ * Represents a single upgrade
+ *
+ * @see UpgradeList
+ * @see BlockUpgradesComponent
+ */
+@MappedContainer
+public class UpgradeInfo {
+    /**
+     * How much the upgrade will cost
+     */
+    private int cost;
+    /**
+     * A mapping between the fields to set, and how much to change them by.
+     * Eg, <code>"power": 5</code> will try to increase the field <code>power</code> by 5
+     */
+    private Map<String, Number> values;
+
+    public int getCost() {
+        return cost;
+    }
+
+    public Map<String, Number> getValues() {
+        return values;
+    }
+}
---
+++ src/main/java/org/terasology/gooeyDefence/upgrading/UpgradeList.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gooeyDefence.upgrading;
+
+import org.terasology.reflection.MappedContainer;
+
+import java.util.List;
+
+/**
+ * Represents a sequence of upgrades.
+ *
+ * @see UpgradeInfo
+ * @see BlockUpgradesComponent
+ */
+@MappedContainer
+public class UpgradeList {
+    /**
+     * The name of this sequence of upgrades
+     */
+    private String upgradeName;
+    /**
+     * The list of upgrade stages.
+     * Order is important, and should be ordered such that 0 is first.
+     */
+    private List<UpgradeInfo> stages;
+
+    public List<UpgradeInfo> getStages() {
+        return stages;
+    }
+
+    public String getUpgradeName() {
+        return upgradeName;
+    }
+
+}
