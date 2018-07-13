a3ff7df4776359d3b610def3187690dcb1a98e73
Quinn Roberts <quinn@jelly-snakes.com>
2018-07-13 13:39:23 +1000

    Change UpgradingSystem to use parsers when retriving values. Also change to use `List<String>` not `Map<String, String>` for values. This does require the lists to maintain order.

--- src/main/java/org/terasology/gooeyDefence/upgrading/UpgradingSystem.java
+++ src/main/java/org/terasology/gooeyDefence/upgrading/UpgradingSystem.java
@@ -17,6 +17,7 @@ package org.terasology.gooeyDefence.upgrading;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.terasology.engine.bootstrap.ClassMetaLibrary;
 import org.terasology.entitySystem.Component;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
@@ -25,11 +26,16 @@ import org.terasology.entitySystem.metadata.ComponentLibrary;
 import org.terasology.entitySystem.metadata.ComponentMetadata;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.gooeyDefence.ui.componentParsers.BaseParser;
 import org.terasology.registry.In;
 import org.terasology.registry.Share;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Handles applying an upgrade to a component
@@ -40,11 +46,26 @@ public class UpgradingSystem extends BaseComponentSystem {
     private static final Logger logger = LoggerFactory.getLogger(UpgradingSystem.class);
     @In
     private EntityManager entityManager;
+    @In
+    private ClassMetaLibrary classMetaLibrary;
     private ComponentLibrary componentLibrary;
+    private Map<Class, BaseParser> parserMap = new HashMap<>();
 
     @Override
     public void postBegin() {
         componentLibrary = entityManager.getComponentLibrary();
+
+        /* Build a list of all the parsers */
+        for (Class<? extends BaseParser> parserClass : classMetaLibrary.getSubtypesOf(BaseParser.class)) {
+            try {
+                BaseParser parser = parserClass.newInstance();
+                parserMap.put(parser.getComponentClass(), parser);
+            } catch (IllegalAccessException | InstantiationException e) {
+                if (parserClass != DefaultParser.class) {
+                    throw new IllegalArgumentException("Could not create parser of type " + parserClass.getSimpleName());
+                }
+            }
+        }
     }
 
     /**
@@ -103,6 +124,7 @@ public class UpgradingSystem extends BaseComponentSystem {
      * @param value     The value to set the field to
      */
     private void setField(ComponentFieldMetadata field, Component component, Number value) {
+        logger.info((field.getType() == int.class) + "");
         switch (field.getType().getSimpleName()) {
             case "int":
                 field.setValue(component, (int) field.getValue(component) + value.intValue());
@@ -130,20 +152,101 @@ public class UpgradingSystem extends BaseComponentSystem {
         }
     }
 
+
+    public <T extends Component> List<String> getComponentFields(T component) {
+        return getComponentFields(component, true);
+    }
+
+    private <T extends Component> List<String> getComponentFields(T component, boolean formatted) {
+        if (component == null) {
+            return Collections.emptyList();
+        }
+        BaseParser parser = parserMap.getOrDefault(component.getClass(), new DefaultParser(component));
+        return (formatted ? parser.getFields().values()
+                : parser.getFields().keySet())
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
     /**
      * Get a mapping between all the fields and their values for a component.
      *
      * @param component The component to get all fields on
      * @return All the fields and their values, as strings.
      */
-    public Map<String, String> getComponentValues(Component component) {
-        ComponentMetadata<? extends Component> metadata = componentLibrary.getMetadata(component);
-        Map<String, String> result = new HashMap<>();
-        if (metadata != null) {
-            for (ComponentFieldMetadata<?, ?> field : metadata.getFields()) {
-                result.put(field.getName(), String.valueOf(field.getValue(component)));
+    public <T extends Component> List<String> getComponentValues(T component) {
+        if (component == null) {
+            return Collections.emptyList();
+        }
+
+        List<String> fields = getComponentFields(component, false);
+        List<String> values = new ArrayList<>(fields.size());
+        ComponentMetadata<T> metadata = componentLibrary.getMetadata(component);
+        BaseParser parser = parserMap.getOrDefault(component.getClass(), new DefaultParser());
+
+        for (String field : fields) {
+            ComponentFieldMetadata<T, ?> fieldMetadata = metadata.getField(field);
+            values.add(parser.handleField(field, fieldMetadata.getValue(component)));
+        }
+
+        return values;
+    }
+
+    public <T extends Component> List<String> getComponentUpgrades(T component, UpgradeInfo upgradeInfo) {
+        if (component == null) {
+            return Collections.emptyList();
+        }
+
+        List<String> fields = getComponentFields(component, false);
+        List<String> upgrades = new ArrayList<>(fields.size());
+        BaseParser parser = parserMap.getOrDefault(component.getClass(), new DefaultParser());
+
+        if (upgradeInfo == null) {
+            return Collections.nCopies(fields.size(), "");
+        }
+
+        for (String field : fields) {
+            Number upgradeValue = upgradeInfo.getValues().getOrDefault(field, null);
+            if (upgradeValue == null) {
+                upgrades.add("");
+            } else {
+                upgrades.add(parser.handleUpgrade(field, upgradeValue));
             }
         }
-        return result;
+
+        return upgrades;
+    }
+
+    public class DefaultParser implements BaseParser {
+        private Component component;
+
+        public DefaultParser() {
+
+        }
+
+        public DefaultParser(Component component) {
+            this.component = component;
+        }
+
+        @Override
+        public Class<? extends Component> getComponentClass() {
+            return Component.class;
+        }
+
+        @Override
+        public Map<String, String> getFields() {
+            Map<String, String> result = new HashMap<>();
+            ComponentMetadata<?> metadata = componentLibrary.getMetadata(component);
+            for (ComponentFieldMetadata<?, ?> field : metadata.getFields()) {
+                result.put(field.getName(), field.getName());
+            }
+            return result;
+        }
+
+        @Override
+        public String handleField(String field, Object value) {
+            return String.valueOf(value);
+        }
     }
 }
