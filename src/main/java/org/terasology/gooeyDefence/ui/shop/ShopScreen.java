8005d095d397bb31f0e87e4d6372364401f1f696
Quinn Roberts <quinn@jelly-snakes.com>
2018-07-17 16:11:48 +1000

    Add a flow layout of all the wares, and methods to add blocks and items

--- src/main/java/org/terasology/gooeyDefence/ui/shop/ShopScreen.java
+++ src/main/java/org/terasology/gooeyDefence/ui/shop/ShopScreen.java
@@ -15,11 +15,49 @@
  */
 package org.terasology.gooeyDefence.ui.shop;
 
+import org.terasology.entitySystem.prefab.Prefab;
+import org.terasology.logic.inventory.ItemComponent;
+import org.terasology.rendering.assets.texture.Texture;
 import org.terasology.rendering.nui.CoreScreenLayer;
+import org.terasology.rendering.nui.layers.ingame.inventory.ItemIcon;
+import org.terasology.rendering.nui.layouts.FlowLayout;
+import org.terasology.utilities.Assets;
+import org.terasology.world.block.Block;
+
+import java.util.Set;
 
 public class ShopScreen extends CoreScreenLayer {
+
+    private FlowLayout wareList;
+
+    private Texture texture = Assets.getTexture("engine:terrain")
+            .orElseGet(() -> Assets.getTexture("engine:default").get());
+
     @Override
     public void initialise() {
+        wareList = find("wareList", FlowLayout.class);
+    }
 
+
+    public void setItems(Set<Prefab> items) {
+        for (Prefab item : items) {
+            ItemComponent itemComponent = item.getComponent(ItemComponent.class);
+            ItemIcon itemIcon = new ItemIcon();
+
+            itemIcon.setIcon(itemComponent.icon);
+            wareList.addWidget(itemIcon, null);
+        }
+    }
+
+    public void setBlocks(Set<Block> blocks) {
+        for (Block block : blocks) {
+            ItemIcon itemIcon = new ItemIcon();
+
+            itemIcon.setMesh(block.getMeshGenerator().getStandaloneMesh());
+            itemIcon.setMeshTexture(texture);
+            wareList.addWidget(itemIcon, null);
+        }
     }
 }
+
+//            itemIcon.setTooltipLines(Collections.singletonList(new TooltipLine("")));
