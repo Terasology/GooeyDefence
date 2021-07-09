02d5a41c5b26088d120273393c68dd4098def6c6
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:17 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/gooeyDefence/towers/components/TowerEffector.java
+++ src/main/java/org/terasology/gooeyDefence/towers/components/TowerEffector.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.gooeyDefence.towers.components;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.gooeyDefence.towers.EffectCount;
 import org.terasology.gooeyDefence.towers.EffectDuration;
 
@@ -28,7 +15,7 @@ import org.terasology.gooeyDefence.towers.EffectDuration;
  * @see TowerCore
  * @see TowerTargeter
  */
-public abstract class TowerEffector implements Component {
+public abstract class TowerEffector implements Component<TowerEffector> {
     /**
      * The amount of power that the effector requires
      */
--- src/main/java/org/terasology/gooeyDefence/upgrading/BlockUpgradesComponent.java
+++ src/main/java/org/terasology/gooeyDefence/upgrading/BlockUpgradesComponent.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.gooeyDefence.upgrading;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.List;
 
@@ -25,7 +12,7 @@ import java.util.List;
  * @see UpgradeList
  * @see UpgradingSystem
  */
-public class BlockUpgradesComponent implements Component {
+public class BlockUpgradesComponent implements Component<BlockUpgradesComponent> {
     /**
      * The component the upgrades will apply to.
      * Should be the name without the <code>Component</code> ending.
