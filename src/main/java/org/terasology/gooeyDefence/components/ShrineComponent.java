ad483e638322c631aae2a317b0eaa4c8ace3b1fb
thierrywong <wong.t@sfeir.com>
2023-12-15 23:35:17 +0100

    Refactor Components without attributes to EmptyComponent

--- src/main/java/org/terasology/gooeyDefence/components/ShrineComponent.java
+++ src/main/java/org/terasology/gooeyDefence/components/ShrineComponent.java
@@ -3,16 +3,12 @@
 package org.terasology.gooeyDefence.components;
 
 import org.terasology.engine.world.block.ForceBlockActive;
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * Component for the central shrine entity.
  * Used as a filter or flag component.
  */
 @ForceBlockActive
-public class ShrineComponent implements Component<ShrineComponent> {
-    @Override
-    public void copyFrom(ShrineComponent other) {
-
-    }
+public class ShrineComponent extends EmptyComponent<ShrineComponent> {
 }
